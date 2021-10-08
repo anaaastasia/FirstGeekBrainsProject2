274c5818d342eb3dbb1d86229339db288231c82a
anaaastasia <anaaastasiaan@iclod.com>
2021-10-08 22:09:18 +0300

    Выполненная первая домашняя работа

---
+++ src/FirstLesson.java
@@ -0,0 +1,12 @@
+
+public class FirstLesson {
+    public static void main(String[] args) {
+        System.out.println("Введите первое число");
+        System.out.println("Введите второе число");
+
+
+
+
+
+    }
+}
