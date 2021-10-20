59b056ae8e9ce608288263471390b0e14e95d1bf
anaaastasia <anaaastasiaan@iclod.com>
2021-10-20 20:11:44 +0300

    Выполненная домашняя работа 3

--- src/HomeWorkApp.java
+++ src/HomeWorkApp.java
@@ -1,42 +1,63 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class HomeWorkApp {
     public static void main(String[] args) {
-        within10and20(5, 6);
-        isPositiveOrNegative(10);
-        isNegative(7);
-        printWordTimes();
+        invertArray();
+        fillArray();
+        changeArray();
+        fillDiagonal();
+        NewArray();
     }
 
-    public static boolean within10and20(int a, int b) {
-        int sum = a + b;
-        if (sum <= 10 && sum >= 20) {
-            return true;
+    public static void invertArray() {
+        int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        for (int i = 0; i < arr.length; i++)
+        if (arr[i] == 1) {
+            arr[i] = 0;
         } else {
-            return false;
-        }
+            arr[i] = 1;
 
-    public static void isPositiveOrNegative(int a){
-            ;
-            if (a < 0) {
-                System.out.println("отрицательное");
-            } else {
-                System.out.println("Положительное");
-            }
-
-            public static boolean isNegative ( int a){
-                if (a < 0) ;
-                {
-                    return true;
-                }
-                return false;
-            }
-
-    public static void printWordTimes(); {
-        for (int i = 0; i = 5; i++) {
-            System.out.println("Привет");
+            System.out.println(Arrays.toString(arr));
         }
     }
+
+    public static void fillArray() {
+        int[] arr = new int[100];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+
+    public static void changeArray() {
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) ;
+            {
+                System.out.println(arr[i] = i * 2);
+            }
+
+        }
+    }
+
+    public static void fillDiagonal() {
+        int[][] arr = new int[4][4];
+        for (int i = 0; i < arr.length; i++)
+            arr[i][i] = 1; {
+        }
+    }
+
+    public static int[] NewArray(int initialValue, int Len)
+    {
+        int[] arr = new int[len];
+        for (int i = 0; i < arr.length; i++)
+        {
+            arr[i] = initialValue;
+        }
+        return arr;
+
+}
 
 
 
